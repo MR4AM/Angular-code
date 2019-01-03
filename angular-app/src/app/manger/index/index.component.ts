1a764c94be9e21334c577af6519f28d6f0ac6f87
limenghui <1479856243@qq.com>
2019-01-03 19:09:27 +0800

    201901031909

--- angular-app/src/app/manger/index/index.component.ts
+++ angular-app/src/app/manger/index/index.component.ts
@@ -23,6 +23,23 @@ const leftNav=[
 export class IndexComponent implements OnInit {
   topNav=topNav;
   leftNav=leftNav;
+  tableData: any[] = [{
+    name: '水爷',
+    date: '2017-08-19',
+    address: '上海市普陀区金沙江路 1518 弄',
+  }, {
+    name: '水爷',
+    date: '2017-08-20',
+    address: '上海市普陀区金沙江路 1518 弄',
+  }, {
+    name: '水爷',
+    date: '2017-08-21',
+    address: '上海市普陀区金沙江路 1518 弄',
+  }, {
+    name: '水爷',
+    date: '2017-08-22',
+    address: '上海市普陀区金沙江路 1510 弄',
+  }]
   constructor() { }
 
   ngOnInit() {
